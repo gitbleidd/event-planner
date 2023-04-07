26484d0b3dbe3c23afe6edd95ba76b31ae22e0e4
gitbleidd <adonis7952@hotmail.com>
2023-04-07 16:43:07 +0500

    feat: add registration, auth with jwt

---
+++ src/backend/EventPlanner/EventPlanner.App/Authentication/JwtManager.cs
@@ -0,0 +1,75 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using EventPlanner.App.Settings;
+using EventPlanner.Data.Security;
+using Microsoft.IdentityModel.Tokens;
+
+namespace EventPlanner.App.Authentication;
+
+public class JwtManager
+{
+    private readonly SecuritySettings _settings;
+    private readonly JwtSecurityTokenHandler _jwtTokenHandler;
+
+    public JwtManager(SecuritySettings settings)
+    {
+        _settings = settings;
+        _jwtTokenHandler = new JwtSecurityTokenHandler();
+    }
+
+    public Tokens GenerateTokens(string userId)
+    {
+        var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+        var tokenKey = Encoding.ASCII.GetBytes(_settings.JwtSecret);
+
+        string jti = Guid.NewGuid().ToString();
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(SecurityConstants.ClaimNames.UserId, userId),
+                new Claim(JwtRegisteredClaimNames.Jti, jti),
+                new Claim(JwtRegisteredClaimNames.Aud, _settings.JwtAudience),
+                new Claim(JwtRegisteredClaimNames.Iss, _settings.JwtIssuer)
+            }),
+            Expires = DateTime.UtcNow.AddDays(_settings.JwtExpireDays),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(tokenKey),
+                SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        SecurityToken token = jwtTokenHandler.CreateToken(tokenDescriptor);
+        string accessToken = jwtTokenHandler.WriteToken(token);
+        string refreshToken = GenerateRefreshToken();
+        return new Tokens
+        {
+            AccessToken = accessToken,
+            RefreshToken = refreshToken,
+            Jti = jti,
+        };
+    }
+
+    private string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[32];
+        using (var rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+
+    public JwtSecurityToken GetParsedJwtToken(string accessToken)
+    {
+        return _jwtTokenHandler.ReadJwtToken(accessToken);
+    }
+
+    public DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+    {
+        var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+        dateTime = dateTime.AddSeconds(unixTimeStamp).ToUniversalTime();
+        return dateTime;
+    }
+}
---
+++ src/backend/EventPlanner/EventPlanner.App/Controllers/EventsContoller.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Mail;
+using EventPlanner.App.Models;
+using EventPlanner.Data;
+using EventPlanner.Data.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlanner.App.Controllers;
+
+[ApiController]
+[Route("api/event")]
+[Authorize]
+public class EventsContoller : ControllerBase
+{
+    private readonly ILogger<UsersController> _logger;
+    private readonly EventPlannerContext _context;
+
+    public EventsContoller(ILogger<UsersController> logger, EventPlannerContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<UserRegisterInfo>> Post(UserRegisterInfo registerInfo)
+    {
+        var mailAddress = new MailAddress(registerInfo.Email);
+        if (!mailAddress.Host.Equals("ibs.ru", StringComparison.OrdinalIgnoreCase))
+        {
+            return BadRequest("The Email host is not 'ibs.ru'.");
+        }
+        
+        var user = new User()
+        {
+            FirstName = registerInfo.FirstName,
+            LastName = registerInfo.LastName,
+            MiddleName = registerInfo.MiddleName,
+            Email = registerInfo.Email
+        };
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        var createdUser = new UserInfo()
+        {
+            Id = user.Id,
+            Email = user.Email,
+            FirstName = user.FirstName,
+            LastName = user.LastName,
+            MiddleName = user.MiddleName
+        };
+        return Created("", createdUser);
+    }
+}
