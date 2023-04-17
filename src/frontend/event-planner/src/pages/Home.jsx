27498056fb485fb0a16d078168169275a73f5f7c
Andrey Shestakov <shest0100@gmail.com>
2023-04-17 14:26:46 +0500

    feat: add the name of the month with a capital letter

--- src/frontend/event-planner/src/pages/Home.jsx
+++ src/frontend/event-planner/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import interactionPlugin from "@fullcalendar/interaction"
 import allLocales from "@fullcalendar/core/locales-all"
+import "./Home.css"
 
 function Home() {
   const navigate = useNavigate();
