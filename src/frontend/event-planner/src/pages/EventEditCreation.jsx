eea76853c08c5c9ae24c15e517be808c5e373c92
Andrey Shestakov <shest0100@gmail.com>
2023-04-17 22:24:48 +0500

    feat: add validation for create event

--- src/frontend/event-planner/src/pages/EventEditCreation.jsx
+++ src/frontend/event-planner/src/pages/EventEditCreation.jsx
@@ -13,67 +13,103 @@ function EventEditCreation() {
     {id: 2, name: "Мастер-класс"},
     {id: 3, name: "Выставка"},
   ]);
-  
-  const handleSubmit = () => {
-    navigate(routes.homeRoute);
-  }
+
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    setValidated(true);
+  };
   
   return (
-
       <FormContainer>
         <h1 class="text-center h1-form">
           Создание мероприятия
         </h1>
-        <Form class="form-center">
+        <Form class="form-center" action={routes.homeRoute} noValidate validated={validated} onSubmit={handleSubmit}>
           <Form.Group>
             <Form.Label>Наименование</Form.Label>
-            <Form.Control type="text" placeholder="Наименование*" />
+            <Form.Control required type="text" placeholder="Наименование*" />
+            <Form.Control.Feedback type="invalid">
+              Заполните наименование мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group >
             <Form.Label>Тип мероприятия</Form.Label>
-            <Form.Select>
+            <Form.Select required>
               {eventTypes.map(event => (
                   <option value={event.id}>{event.name}</option>
               ))}
-            </Form.Select> 
+            </Form.Select>
+            <Form.Control.Feedback type="invalid">
+              Заполните тип мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Организатор</Form.Label>
-            <Form.Control type="text" placeholder="Организатор*" />
+            <Form.Control required type="text" placeholder="Организатор*" />
+            <Form.Control.Feedback type="invalid">
+              Заполните организатора мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Стоимость</Form.Label>
-            <Form.Control type="number" placeholder="Стоимость" />
+            <Form.Control type="number" min={1} placeholder="Стоимость" />
+            <Form.Control.Feedback type="invalid">
+              Стоимость должна быть больше ноля.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Количество мест</Form.Label>
-            <Form.Control type="number" placeholder="Количество мест" />
+            <Form.Control type="number" min={1} placeholder="Количество мест" />
+            <Form.Control.Feedback type="invalid">
+              Количество мест должно быть больше ноля.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Количество гостей, которых можно пригласить</Form.Label>
-            <Form.Control type="number" placeholder="Количество гостей, которых можно пригласить" />
+            <Form.Control required type="number" min={1} placeholder="Количество гостей, которых можно пригласить" />
+            <Form.Control.Feedback type="invalid">
+              Заполните количество дополнительных гостей значением больше ноля.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Дата начала</Form.Label>
-            <Form.Control type="date" placeholder="Дата начала" />
+            <Form.Control required type="date" placeholder="Дата начала" />
+            <Form.Control.Feedback type="invalid">
+              Заполните дату начала мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Дата окончания</Form.Label>
-            <Form.Control type="date" placeholder="Дата окончания" />
+            <Form.Control required type="date" placeholder="Дата окончания" />
+            <Form.Control.Feedback type="invalid">
+              Заполните дату окончания мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Дата окончания регистрации</Form.Label>
-            <Form.Control type="date" placeholder="Дата окончания регистрации" />
+            <Form.Control required type="date" placeholder="Дата окончания регистрации" />
+            <Form.Control.Feedback type="invalid">
+              Заполните дату окончания регистрации на мероприятие.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group>
             <Form.Label>Описание</Form.Label>
-            <Form.Control as="textarea" rows={3} placeholder="Описание" />
+            <Form.Control required as="textarea" rows={3} placeholder="Описание" />
+            <Form.Control.Feedback type="invalid">
+              Заполните описание мероприятия.
+            </Form.Control.Feedback>
           </Form.Group>
           <Form.Group className="d-flex justify-content-between">
             <Button className="mr-auto p-2 my-4">
               <CardImage className="card-image"/> Добавить изображение
             </Button>
-            <Button type="submit" onClick={handleSubmit} className="p-2 align-self-end my-4 col-3">Создать</Button>
+            <Button type="submit" className="p-2 align-self-end my-4 col-3">Создать</Button>
           </Form.Group>
         </Form>
       </FormContainer>
